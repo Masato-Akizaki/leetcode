26f81a1bc4c6cd85b9f75aea7645838b27f40db0
Masato AKIZAKI <m.graceful_9411@icloud.com>
2021-05-30 16:11:47 +0900

    set environment

---
+++ problems/1.two-sum.rb
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=1 lang=ruby
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+    
+end
+# @lc code=end
+
