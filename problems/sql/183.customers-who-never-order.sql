c1cb8af3de5dddce5f99324644835d93af8c2749
Masato AKIZAKI <m.graceful_9411@icloud.com>
2021-07-19 06:19:38 +0900

    solve Customers Who Never Order

---
+++ problems/sql/183.customers-who-never-order.sql
@@ -0,0 +1,19 @@
+--
+-- @lc app=leetcode id=183 lang=mysql
+--
+-- [183] Customers Who Never Order
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+select
+  Name as Customers
+from
+  Customers
+left join
+  Orders
+  on Customers.Id = Orders.CustomerId
+where
+  Orders.Id is null
+-- @lc code=end
+
