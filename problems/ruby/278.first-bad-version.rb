07b6784840b2a8a5f5192cdd29a0cafc21f94014
Masato AKIZAKI <m.graceful_9411@icloud.com>
2021-08-16 07:05:11 +0900

    solve First Bad Version

---
+++ problems/ruby/278.first-bad-version.rb
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=278 lang=ruby
+#
+# [278] First Bad Version
+#
+
+# @lc code=start
+# The is_bad_version API is already defined for you.
+# @param {Integer} version
+# @return {boolean} whether the version is bad
+# def is_bad_version(version):
+
+# @param {Integer} n
+# @return {Integer}
+def first_bad_version(n)
+  return n if n == 1
+  min, max = 1, n
+  middle = (min + max) / 2
+
+  until is_bad_version(middle) && !is_bad_version(middle-1)
+    if is_bad_version(middle)
+      max = middle
+      middle = (min + max) / 2
+    else
+      min = middle + 1
+      middle = (min + max) / 2
+    end
+  end
+  middle
+end
+# @lc code=end
+
