a91940614f5172c9ae8c2b19d386d2ca602b1d49
Masato AKIZAKI <m.graceful_9411@icloud.com>
2021-08-23 07:02:48 +0900

    solve Reverse String

---
+++ problems/ruby/344.reverse-string.rb
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=344 lang=ruby
+#
+# [344] Reverse String
+#
+
+# @lc code=start
+# @param {Character[]} s
+# @return {Void} Do not return anything, modify s in-place instead.
+def reverse_string(s)
+  s.reverse!
+end
+# @lc code=end
+
