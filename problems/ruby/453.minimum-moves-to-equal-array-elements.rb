2a8b80730b561020f4ee9b3dcd4a5f92b2428c65
Masato AKIZAKI <m.graceful_9411@icloud.com>
2021-09-08 06:59:48 +0900

    solve Minimum Moves to Equal Array Elements

--- problems/ruby/453.minimum-moves-to-equal-array-elements.rb
+++ problems/ruby/453.minimum-moves-to-equal-array-elements.rb
@@ -8,15 +8,7 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def min_moves(nums)
-  nums = nums.sort!
-  move_num = 0
-  while nums.uniq.length > 1
-    nums.map!{|x| x += 1}
-    nums[-1] -= 1
-    nums = nums.sort!
-    move_num += 1
-  end
-  move_num
+  nums.sum - (nums.length * nums.min)
 end
 # @lc code=end
 
