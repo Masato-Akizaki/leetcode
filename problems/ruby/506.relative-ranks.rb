2b1948747de0316c9b30e6a26ad3963f94005d90
Masato AKIZAKI <m.graceful_9411@icloud.com>
2021-09-22 06:49:01 +0900

    solve Relative Ranks

---
+++ problems/ruby/506.relative-ranks.rb
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=506 lang=ruby
+#
+# [506] Relative Ranks
+#
+
+# @lc code=start
+# @param {Integer[]} score
+# @return {String[]}
+def find_relative_ranks(score)
+  ranks = ["Gold Medal", "Silver Medal", "Bronze Medal"] + [*"4".."#{score.length}"]
+  p score_ranks = score.sort.reverse.zip(ranks).to_h
+  score.map{ |x| score_ranks[x] }
+end
+# @lc code=end
+
