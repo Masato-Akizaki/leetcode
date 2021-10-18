5e656f3c8c6ec736c93c11d94c75c08c99c6b62e
Masato AKIZAKI <m.graceful_9411@icloud.com>
2021-10-19 07:00:00 +0900

    solve Swap Salary

---
+++ problems/ruby/627.swap-salary.sql
@@ -0,0 +1,16 @@
+--
+-- @lc app=leetcode id=627 lang=mysql
+--
+-- [627] Swap Salary
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+update
+  Salary
+set
+  sex = case when sex = 'm' then 'f'
+             when sex = 'f' then 'm'
+        end
+-- @lc code=end
+
