ae40f6d05153c57743e8864911955f9846beb913
Masato AKIZAKI <m.graceful_9411@icloud.com>
2021-10-20 06:45:42 +0900

    solve Maximum Product of Three Numbers

---
+++ problems/ruby/628.maximum-product-of-three-numbers.rb
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=628 lang=ruby
+#
+# [628] Maximum Product of Three Numbers
+#
+
+# @lc code=start
+# @param {Integer[]} nums
+# @return {Integer}
+def maximum_product(nums)
+  nums = nums.sort
+  [nums.last(3).inject(:*), nums.last * nums[0] * nums[1]].max
+end
+# @lc code=end
+
+"[-100,-2,-3,1]"
+"[-1,-2,-3]"
