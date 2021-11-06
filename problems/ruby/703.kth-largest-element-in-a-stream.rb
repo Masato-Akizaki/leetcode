6fd2d077dea5fc13b68d96ae7029984c6bdcf181
Masato AKIZAKI <m.graceful_9411@icloud.com>
2021-11-07 08:45:06 +0900

    solve Kth Largest Element in a Stream

--- problems/ruby/703.kth-largest-element-in-a-stream.rb
+++ problems/ruby/703.kth-largest-element-in-a-stream.rb
@@ -13,7 +13,7 @@ class KthLargest
 =end
     def initialize(k, nums)
       @k = k
-      @k_val = nums.sort.reverse[0..@k-1]
+      @data = nums.sort.reverse[0...k]
     end
 
 
@@ -22,9 +22,13 @@ class KthLargest
     :rtype: Integer
 =end
     def add(val)
-      @k_val << val
-      @k_val = @k_val.sort.reverse[0..@k-1]
-      return @k_val[-1]
+      if i = @data.bsearch_index{ |x| x < val }
+        @data.insert(i,val)
+      else
+        @data.push(val)
+      end
+      @data.pop if @data.length > @k
+      @data.last
     end
 
 
@@ -35,8 +39,3 @@ end
 # param_1 = obj.add(val)
 # @lc code=end
 
-"["KthLargest","add","add","add","add","add"]\n[[1,[]],[-3],[-2],[-4],[0],[4]]"
-
-"["KthLargest","add","add","add","add","add"]\n[[2,[0]],[-1],[1],[-2],[-4],[3]]"
-
-"["KthLargest","add","add","add","add","add"]\n[[3,[5,-1]],[2],[1],[-1],[3],[4]]"
