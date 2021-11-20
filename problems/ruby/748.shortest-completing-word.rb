83ca750018e0e45d43fece0836651fbb8433137f
Masato AKIZAKI <m.graceful_9411@icloud.com>
2021-11-21 08:53:31 +0900

    solve Shortest Completing Word

--- problems/ruby/748.shortest-completing-word.rb
+++ problems/ruby/748.shortest-completing-word.rb
@@ -12,13 +12,8 @@ def shortest_completing_word(license_plate, words)
   completing_words = []
   letters = license_plate.gsub(/[\d]|[\s]/, "").downcase.chars.tally
   words.each do |word|
-    judge = []
     word_hash = word.chars.tally
-    letters.each do |k,v|
-      next judge << false if word_hash[k].nil?
-      judge << (word_hash[k] >= v)
-    end
-    completing_words << word if judge.all?(true)  
+    completing_words << word if letters.all? {|k,v| word_hash[k] && word_hash[k] >= v}
   end
   min_len = completing_words.map {|a| a.length}.min
   completing_words.find{|x| x.length == min_len}
