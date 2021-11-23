1c86a16ccffb071862194fe01ca24788194b337a
Masato AKIZAKI <m.graceful_9411@icloud.com>
2021-11-24 06:27:05 +0900

    solve Jewels and Stones

---
+++ problems/ruby/771.jewels-and-stones.rb
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=771 lang=ruby
+#
+# [771] Jewels and Stones
+#
+
+# @lc code=start
+# @param {String} jewels
+# @param {String} stones
+# @return {Integer}
+def num_jewels_in_stones(jewels, stones)
+  stones.count(jewels)
+end
+# @lc code=end
+
