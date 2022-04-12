5016ba0f8ff7cd055a2540d8bdba0295a24f94ca
Masato AKIZAKI <m.graceful_9411@icloud.com>
2022-04-12 22:08:45 +0900

    solve Average Salary Excluding the Minimum and Maximum Salary

--- problems/ruby/1491.average-salary-excluding-the-minimum-and-maximum-salary.rb
+++ problems/ruby/1491.average-salary-excluding-the-minimum-and-maximum-salary.rb
@@ -1,18 +1,13 @@
 #
-# @lc app=leetcode id=1486 lang=ruby
+# @lc app=leetcode id=1491 lang=ruby
 #
-# [1486] XOR Operation in an Array
+# [1491] Average Salary Excluding the Minimum and Maximum Salary
 #
 
 # @lc code=start
-# @param {Integer} n
-# @param {Integer} start
-# @return {Integer}
-def xor_operation(n, start)
-    arr = []
-    for i in 0...n do
-        arr << start + (i * 2)
-    end
-    arr.inject(:^)
+# @param {Integer[]} salary
+# @return {Float}
+def average(salary)
+    salary.sort[1..-2].sum(0.0) / (salary.length - 2)
 end
 # @lc code=end
