d62f8799bf9bec30c350927325da9ba0c3bd046b
Masato AKIZAKI <m.graceful_9411@icloud.com>
2022-04-27 22:21:21 +0900

    solve Patients With a Condition

---
+++ problems/sql/1527.patients-with-a-condition.sql
@@ -0,0 +1,19 @@
+--
+-- @lc app=leetcode id=1527 lang=mysql
+--
+-- [1527] Patients With a Condition
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+select
+    patient_id
+    , patient_name
+    , conditions
+from
+    Patients
+where
+    conditions like "DIAB1%"
+    or conditions like "% DIAB1%"
+-- @lc code=end
+
