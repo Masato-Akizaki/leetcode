0c20cb65f8b77b7e17a59e07d038ee14e202b0cb
Masato AKIZAKI <m.graceful_9411@icloud.com>
2022-05-13 23:59:59 +0900

    solve Crawler Log Folder

--- problems/ruby/1598.crawler-log-folder.rb
+++ problems/ruby/1598.crawler-log-folder.rb
@@ -1,18 +1,23 @@
 #
-# @lc app=leetcode id=1592 lang=ruby
+# @lc app=leetcode id=1598 lang=ruby
 #
-# [1592] Rearrange Spaces Between Words
+# [1598] Crawler Log Folder
 #
 
 # @lc code=start
-# @param {String} text
-# @return {String}
-def reorder_spaces(text)
-    total_spaces = text.count(" ")
-    arr = text.split(" ")
-    return arr.join + (" " * total_spaces) if arr.length == 1
-    between_spaces = total_spaces / (arr.length - 1)
-    extra_spaces = total_spaces % (arr.length - 1)
-    arr.join(" " * between_spaces) + (" " * extra_spaces)
+# @param {String[]} logs
+# @return {Integer}
+def min_operations(logs)
+    hierarchy = 0
+    logs.each do |operate|
+        if operate == "./"
+            next
+        elsif operate == "../"
+            hierarchy -= 1 if hierarchy > 0
+        else
+            hierarchy += 1
+        end
+    end 
+    hierarchy
 end
 # @lc code=end
