f568ba757721a6b1c1a6508ac347a947692a240d
Masato AKIZAKI <m.graceful_9411@icloud.com>
2022-05-31 23:06:53 +0900

    solve Count of Matches in Tournament

---
+++ problems/ruby/1688.count-of-matches-in-tournament.rb
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=1688 lang=ruby
+#
+# [1688] Count of Matches in Tournament
+#
+
+# @lc code=start
+# @param {Integer} n
+# @return {Integer}
+def number_of_matches(n)
+    n - 1
+end
+# @lc code=end
