7e3994fef2ae94beae6fd4d2156526535c75524a
Masato AKIZAKI <m.graceful_9411@icloud.com>
2022-06-01 21:36:47 +0900

    solve Daily Leads and Partners

---
+++ problems/sql/1693.daily-leads-and-partners.sql
@@ -0,0 +1,19 @@
+--
+-- @lc app=leetcode id=1693 lang=mysql
+--
+-- [1693] Daily Leads and Partners
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+select
+    date_id
+    , make_name
+    , count(distinct lead_id) as unique_leads
+    , count(distinct partner_id) as unique_partners
+from
+    DailySales
+group by 1,2
+
+-- @lc code=end
+
