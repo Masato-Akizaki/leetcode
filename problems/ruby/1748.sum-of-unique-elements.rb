2eb4cc12163a4e70e4e5d50f56adc9ce7c6bb813
Masato AKIZAKI <m.graceful_9411@icloud.com>
2022-06-14 20:22:51 +0900

    solve Sum of Unique Elements

---
+++ problems/ruby/1748.sum-of-unique-elements.rb
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1748 lang=ruby
+#
+# [1748] Sum of Unique Elements
+#
+
+# @lc code=start
+# @param {Integer[]} nums
+# @return {Integer}
+def sum_of_unique(nums)
+    nums.tally.select{|_, v| v == 1}.keys.sum
+end
