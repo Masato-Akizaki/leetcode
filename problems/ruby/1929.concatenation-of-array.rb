59f4b69c4fb1499b98b19b97b84a398712527e92
Masato AKIZAKI <m.graceful_9411@icloud.com>
2022-07-23 11:08:56 +0900

    solve Concatenation of Array

---
+++ problems/ruby/1929.concatenation-of-array.rb
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=1929 lang=ruby
+#
+# [1929] Concatenation of Array
+#
+
+# @lc code=start
+# @param {Integer[]} nums
+# @return {Integer[]}
+def get_concatenation(nums)
+    nums + nums
+end
+# @lc code=end
