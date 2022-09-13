d31f5316e40e0c1391247b607726fe2dc49a8b48
Masato AKIZAKI <m.graceful_9411@icloud.com>
2022-09-13 20:31:48 +0900

    solve Most Frequent Number Following Key In an Array

--- problems/ruby/2190.most-frequent-number-following-key-in-an-array.rb
+++ problems/ruby/2190.most-frequent-number-following-key-in-an-array.rb
@@ -1,18 +1,14 @@
 #
-# @lc app=leetcode id=2185 lang=ruby
+# @lc app=leetcode id=2190 lang=ruby
 #
-# [2185] Counting Words With a Given Prefix
+# [2190] Most Frequent Number Following Key In an Array
 #
 
 # @lc code=start
-# @param {String[]} words
-# @param {String} pref
+# @param {Integer[]} nums
+# @param {Integer} key
 # @return {Integer}
-def prefix_count(words, pref)
-    count = 0
-    words.each do |w|
-      count += 1 if w.start_with?(pref)
-    end
-    count
+def most_frequent(nums, key)
+  nums.select.with_index{|_, i| nums[i-1] == key}.tally.sort_by{|_, v| v}.to_h.keys[-1]
 end
 # @lc code=end
