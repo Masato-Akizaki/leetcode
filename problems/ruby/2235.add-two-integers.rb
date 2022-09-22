182331474fc38dc8db76ff978b5a6723a411c279
Masato AKIZAKI <m.graceful_9411@icloud.com>
2022-09-22 15:51:26 +0900

    solve Add Two Integers

---
+++ problems/ruby/2235.add-two-integers.rb
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=2235 lang=ruby
+#
+# [2235] Add Two Integers
+#
+
+# @lc code=start
+# @param {Integer} num1
+# @param {Integer} num2
+# @return {Integer}
+def sum(num1, num2)
+    num1 + num2
+end
+# @lc code=end
