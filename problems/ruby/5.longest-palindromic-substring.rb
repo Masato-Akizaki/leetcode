f7175e8bb8ba1f07ab6c25a42ceba45d1208cc6e
Masato AKIZAKI <m.graceful_9411@icloud.com>
2022-11-11 22:39:43 +0900

    solve Longest Palindromic Substring

---
+++ problems/ruby/5.longest-palindromic-substring.rb
@@ -0,0 +1,40 @@
+#
+# @lc app=leetcode id=5 lang=ruby
+#
+# [5] Longest Palindromic Substring
+#
+
+# @lc code=start
+# @param {String} s
+# @return {String}
+def longest_palindrome(s)
+  return "" if s.empty?
+  
+  head, tail = 0, 0
+  
+  i = 0
+  while i < s.size
+      len1 = find_palindrome(s, i, i)
+      len2 = find_palindrome(s, i, i + 1)
+      len = [len1, len2].max
+      if tail - head < len
+          head = i - (len - 1) / 2
+          tail = i + len / 2
+      end
+      
+      i += 1
+  end
+  
+  s[head..tail]
+end
+
+def find_palindrome(s, left, right)
+  l, r = left, right
+  while 0 <= l && r < s.size && s[l] == s[r]
+      l -= 1
+      r += 1
+  end
+  r - l - 1
+end
+# @lc code=end
+
