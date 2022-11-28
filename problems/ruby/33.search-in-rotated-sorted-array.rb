0f274b86382ba4f9c7ba78b38340dba70a4c2324
Masato AKIZAKI <m.graceful_9411@icloud.com>
2022-11-28 21:34:00 +0900

    solve Search in Rotated Sorted Array

---
+++ problems/ruby/33.search-in-rotated-sorted-array.rb
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=33 lang=ruby
+#
+# [33] Search in Rotated Sorted Array
+#
+
+# @lc code=start
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer}
+def search(nums, target)
+  nums.index(target) || -1
+end
+# @lc code=end
+
