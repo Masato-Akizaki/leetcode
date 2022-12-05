a868ba37f119341e6db4b3356794d13659563df7
Masato AKIZAKI <m.graceful_9411@icloud.com>
2022-12-05 21:49:31 +0900

    solve Jump Game II

---
+++ problems/ruby/45.jump-game-ii.rb
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=45 lang=ruby
+#
+# [45] Jump Game II
+#
+
+# @lc code=start
+# @param {Integer[]} nums
+# @return {Integer}
+def jump(nums)
+  current_jump = 0
+  last_jump = 0
+  step = 0
+  
+  (nums.size - 1).times do |i|
+      current_jump = [current_jump, i + nums[i]].max
+      if i == last_jump
+      step += 1
+      last_jump = current_jump
+      end
+  end
+  step
+end
+# @lc code=end
+
