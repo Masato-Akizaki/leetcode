131aaa22407d71ab3d7554d66610bb1fe777ef09
Masato AKIZAKI <m.graceful_9411@icloud.com>
2022-12-17 13:35:18 +0900

    solve Unique Paths

---
+++ problems/ruby/62.unique-paths.rb
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=62 lang=ruby
+#
+# [62] Unique Paths
+#
+
+# @lc code=start
+# @param {Integer} m
+# @param {Integer} n
+# @return {Integer}
+def unique_paths(m, n)
+  matrix = Array.new(n) {Array.new(m)} 
+
+	(0...m).each do |col|
+		matrix[0][col] = 1
+	end
+
+	(0...n).each do |row|
+		matrix[row][0] = 1
+	end
+
+	(1...n).each do |row|
+		(1...m).each do |col|
+			matrix[row][col] = matrix[row][col-1] + matrix[row-1][col]
+		end
+	end
+	matrix[-1][-1]
+end
+# @lc code=end
+
