b771814eb51c9d0e138673ba07356ccfefc909d7
Masato AKIZAKI <m.graceful_9411@icloud.com>
2022-12-22 23:35:54 +0900

    solve Search a 2D Matrix

---
+++ problems/ruby/74.search-a-2-d-matrix.rb
@@ -0,0 +1,43 @@
+#
+# @lc app=leetcode id=74 lang=ruby
+#
+# [74] Search a 2D Matrix
+#
+
+# @lc code=start
+# @param {Integer[][]} matrix
+# @param {Integer} target
+# @return {Boolean}
+def search_matrix(matrix, target)
+  return false if matrix.empty? || matrix.first.empty?
+  starts = matrix.map{|row| row.first}
+  
+  pre = nil
+  pre_idx = nil
+  starts.each_with_index do |start,i|
+      case pre
+          when nil 
+          then
+              if start < target
+                  pre = start
+                  pre_idx = i
+              elsif start == target
+                  return true
+              else
+                  return false
+              end
+          else
+              if start < target
+                  pre = start
+                  pre_idx = i
+              elsif start == target
+                  return true
+              else
+                  break
+              end
+      end  
+    end
+  return matrix[pre_idx].include? target
+end
+# @lc code=end
+
