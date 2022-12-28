916e4f199c3b35016510a684fca7bc3e156c557f
Masato AKIZAKI <m.graceful_9411@icloud.com>
2022-12-28 23:14:08 +0900

    solve Search in Rotated Sorted Array II

---
+++ problems/ruby/81.search-in-rotated-sorted-array-ii.rb
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=81 lang=ruby
+#
+# [81] Search in Rotated Sorted Array II
+#
+
+# @lc code=start
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Boolean}
+def search(nums, target)
+  nums.include?(target) 
+end
+# @lc code=end
+
